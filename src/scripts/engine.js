529266311e7856e942f3d78f7553a2ff8f047f1b
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-19 16:57:22 -0300

    feat: add show/hide piano keys

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
 let audio = new Audio("./src/tunes/a.wav");
 let mapedKeys = [];
@@ -27,7 +28,10 @@ document.addEventListener("keydown", (e) => {
 
 const handleVolume = (e) => {
   audio.volume = e.target.value;
-}
+};
+const showHideKeys = (e) => {
+  pianoKeys.forEach(key => key.classList.toggle("hide"))
+};
 
 volumeSlider.addEventListener("input", handleVolume);
-
+keysCheck.addEventListener("click", showHideKeys);
