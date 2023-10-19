9ef43e2ddd675a9e30669532ad4d85f4e6131139
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-19 16:33:57 -0300

    feat: map keyboard to keys

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -9,4 +9,9 @@ const playTune = (key) => {
 pianoKeys.forEach((key) => {
   console.log(key.dataset.key);
   key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+document.addEventListener("keydown", (e) => {
+  playTune(e.key);
+  console.log(e.key);
 })
